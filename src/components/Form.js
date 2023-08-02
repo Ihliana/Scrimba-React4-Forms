f7f6341f4fed667b96c14933f5c7399be870a4a5
Ihliana <Ihnatco@protonmail.com>
2023-08-02 10:01:31 +0200

    Form state object
    create a form state object to handle the inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,42 @@ import React from "react"
 
 
 export default function Form(){
-    const [firstName, setFirstName] = React.useState("")
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email: ""
+    })
 
+       /**
+     * Challenge: add an email field/state to the form
+     */
+    
   
     function handleChange(event){
-        setFirstName(event.target.values)
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData, 
+                [event.target.name] : event.target.value
+            }
+        })
     }
 
     return(
         <form>
             <input type="text"
                     placeholder="First Name"
-                    onChange={handleChange} />
+                    onChange={handleChange} 
+                    name="firstName"/>
+
+            <input type='text'
+                    placeholder="Last Name"
+                    onChange={handleChange}
+                    name="lastName" />
+
+            <input type="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    name="email" />
         </form>
     )
 }
